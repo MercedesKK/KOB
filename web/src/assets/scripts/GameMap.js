45f26019ff8e1de8652926b1bcdd093e2707e617
zjk <2455650395@qq.com>
2023-02-19 21:51:56 +0800

    蛇移动功能实现

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { GameObject } from "./GameObject";
 import { Wall } from "./Wall";
+import { Snake } from './Snake';
 
 export class GameMap extends GameObject {
     constructor(ctx, parent) {
@@ -9,10 +10,25 @@ export class GameMap extends GameObject {
         this.parent = parent;
         this.L = 0;
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
 
         this.inner_walls_count = 20;
         this.walls = [];
+
+        this.snakes = [
+            new Snake({
+                id: 0,
+                color: "#4876EC",
+                r: this.rows - 2,
+                c: 1,
+            }, this),
+            new Snake({
+                id: 1,
+                color: "#F94848",
+                r: 1,
+                c: this.cols - 2,
+            }, this),
+        ]
     }
 
     // 判断连通性
@@ -60,7 +76,7 @@ export class GameMap extends GameObject {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
 
-                if (g[c][r] || g[r][c]) {
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) {
                     continue;
                 }
 
@@ -68,7 +84,7 @@ export class GameMap extends GameObject {
                     continue;
                 }
 
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -90,6 +106,39 @@ export class GameMap extends GameObject {
         return true;
     }
 
+    add_listening_events() {
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') {
+                snake0.set_direction(0);
+            }
+            else if (e.key === 'd') {
+                snake0.set_direction(1);
+            }
+            else if (e.key === 's') {
+                snake0.set_direction(2);
+            }
+            else if (e.key === 'a') {
+                snake0.set_direction(3);
+            }
+            else if (e.key === 'i') {
+                snake1.set_direction(0);
+            }
+            else if (e.key === 'l') {
+                snake1.set_direction(1);
+            }
+            else if (e.key === 'k') {
+                snake1.set_direction(2);
+            }
+            else if (e.key === 'j') {
+                snake1.set_direction(3);
+            }
+        });
+    }
+
     start() {
         for (let i = 0; i < 1000; i++) {
             if (this.creatWalls()) {
@@ -97,6 +146,7 @@ export class GameMap extends GameObject {
             }
         }
 
+        this.add_listening_events();
     }
 
     update_size() {
@@ -105,8 +155,53 @@ export class GameMap extends GameObject {
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_valid(cell) { // 检测目标位置是否合法，没有撞到蛇的身体和障碍物
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c) {
+                return false;
+            }
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) {
+                k--;
+            }
+            for (let i = 0; i < k; i++) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // 判断两蛇是否准好下一回合
+    check_ready() {
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") {
+                return false;
+            }
+            if (snake.direction === -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 进入下一回合
+    next_step() {
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
+    }
+
     update() {
         this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
     }
 
---
+++ web/src/assets/scripts/Snake.js
@@ -0,0 +1,141 @@
+import { GameObject } from "./GameObject";
+import { Cell } from "./Cell";
+
+export class Snake extends GameObject {
+    constructor(info, gamemap) {
+        super();
+
+        this.id = info.id;
+        this.color = info.color;
+        this.gamemap = gamemap;
+
+        this.cells = [new Cell(info.r, info.c)]; // 存放蛇的身体,0存放蛇头
+        this.next_cell = null; // 下一步目标位置
+
+        this.speed = 5; // 每秒5个格子 
+        this.direction = -1; // 0,1,2,3代表上右下左, -1表示没有指令
+        this.status = "idle"; // idle表示静止，move表示移动，die表示死亡
+
+        this.dr = [-1,0,1,0];
+        this.dc = [0,1,0,-1];
+
+        this.step = 0; // 当前回合数
+
+        this.eps = 1e-2;
+    }
+
+    start() {
+
+    }
+
+    set_direction(d) {
+        this.direction = d;
+    }
+
+    // 检测当前回合蛇长度是否增加
+    check_tail_increasing() {
+        if (this.step <= 10) {
+            return true;
+        }
+        if (this.step % 3 === 1) {
+            return true;
+        }
+        return false;
+    }
+
+    // 蛇走下一步
+    next_step() {
+        const d = this.direction;
+        this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0].c + this.dc[d]);
+        this.direction = -1; // 清空操作
+        this.status = "move";
+        this.step++;
+
+        const k = this.cells.length;
+        for (let i = k; i > 0; i--) {
+            this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));
+        }
+
+        if (!this.gamemap.check_valid(this.next_cell)) {
+            this.status = "die";
+        }
+    }
+
+
+
+
+    update_move() {
+        const dx = this.next_cell.x - this.cells[0].x;
+        const dy = this.next_cell.y - this.cells[0].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        // 走到目标点
+        if (distance < this.eps) {
+            this.cells[0] = this.next_cell;
+            this.next_cell = null;
+            this.status = "idle";
+
+            if (!this.check_tail_increasing()) {
+                this.cells.pop();
+            }
+        }
+        else { // 移动过程
+            const move_distance = this.speed * this.timedelta / 1000; // 每帧走过的距离
+
+            // 蛇头移动
+            this.cells[0].x += move_distance * dx / distance;
+            this.cells[0].y += move_distance * dy / distance;
+
+            // 蛇尾移动
+            if (!this.check_tail_increasing()) {
+                const k = this.cells.length;
+                const tail = this.cells[k - 1], tail_target = this.cells[k - 2];
+                const tail_dx = tail_target.x - tail.x;
+                const tail_dy = tail_target.y - tail.y;
+                tail.x += move_distance * tail_dx / distance;
+                tail.y += move_distance * tail_dy / distance;
+            }
+        }
+
+
+
+    }
+
+    update() {
+        if (this.status === "move") {
+            this.update_move();
+        }
+        this.render();
+    }
+
+    render() {
+        const L = this.gamemap.L;
+        const ctx = this.gamemap.ctx;
+
+        ctx.fillStyle = this.color;
+
+        if (this.status === "die") {
+            ctx.fillStyle = "white";
+        }
+
+        for (const cell of this.cells) {
+            ctx.beginPath();
+            ctx.arc(cell.x * L, cell.y * L, L / 2  * 0.8, 0, Math.PI * 2);
+            ctx.fill();
+        }
+
+        for (let i = 1; i < this.cells.length; i++) {
+            const a = this.cells[i - 1], b = this.cells[i];
+            if (Math.abs(a.x - b.x) < this.eps && Math.abs(a.y - b.y) < this.eps) {
+                continue;
+            }
+
+            if (Math.abs(a.x - b.x) < this.eps) {
+                ctx.fillRect((a.x - 0.5 * 0.8) * L, Math.min(a.y, b.y) * L, L * 0.8, Math.abs(a.y - b.y) * L);
+            }
+            else {
+                ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.5 * 0.8) * L, Math.abs(a.x - b.x) * L, L * 0.8);    
+            }
+        }
+    }
+}
